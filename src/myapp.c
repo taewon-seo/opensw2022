b6f0122d3ec734dbde529a63484864b8a195b161
Taewon Seo <stw122899@gmail.com>
2022-11-03 17:57:40 +0900

    myapp.c add comment

--- src/myapp.c
+++ src/myapp.c
@@ -1,12 +1,13 @@
 #include "myfunc.h"
 
 int main() {
-    
+    // call functions 3 times...
     for (int i=0; i<3; i++) {
         foo();
         goo();
     }
-    
+
+    // call zoo three times...
     for (int i=0; i<13; i++){
         zoo();
     }
